ae4517cd21680f38a29a7b456df43090bfd884c2
André Luís Lopes da Silva <78765404+Andre-Luis-Lopes-da-Silva@users.noreply.github.com>
2024-10-19 17:07:25 -0300

    Update hello.java

--- hello.java
+++ hello.java
@@ -1,5 +1,7 @@
-public class Main {
-    public static void main(String[] args) {
-        System.out.println("Hello, World!");
+public class Main {  // Class
+    public static void main(String[] args) {   // Method 
+        System.out.println("Hello, World!"); // It moves the cursor to the next line
+        System.out.println(); // It creates a empty line
+        System.out.print("Hello, World!"); // It do not moves the cursor to the next line
     }
 }
